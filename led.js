8221ef107651680a1ed5ba2820f57645c657a1e8
Dave Conway-Jones <conway@uk.ibm.com>
2019-09-08 17:32:31 +0100

    try to clarify layout. Tidy spacing slightly.
    Add on off to imporatable examples

--- led.js
+++ led.js
@@ -8,15 +8,15 @@ module.exports = function(RED) {
 	 */
     function LEDNode(config) {
         try {
-			var ui = undefined; 
-			if(ui === undefined) {
+			var ui = undefined;
+			if (ui === undefined) {
                 ui = RED.require("node-red-dashboard")(RED);
             }
 
-            RED.nodes.createNode(this, config);                       
+            RED.nodes.createNode(this, config);
 
 			var node = this;
-			 
+
 			this.colorForValue = config.colorForValue.map(function(colorForValue) {
 				return {
 					color: colorForValue.color,
@@ -37,22 +37,23 @@ module.exports = function(RED) {
 			};
 
 			if (ledUtility.checkConfig(config, node)) {
-	            var done = ui.addWidget({                   
-					node: node,    
+	            var done = ui.addWidget({
+					node: node,
 					width: config.width || config.group.width || undefined,
 					height: config.height || 1,
-	                format: ledUtility.HTML(config, ledUtility.ledStyle('gray', false)), 
-	                group: config.group,  
+	                format: ledUtility.HTML(config, ledUtility.ledStyle('gray', false)),
+	                group: config.group,
 	                templateScope: "local",
 	                order: config.order,
 	                beforeEmit: ledUtility.beforeEmit(node, RED),
+                    emitOnlyNewValues: false,
 	                initController: ledUtility.initController
 				});
 
 				node.on("close", done);
-			}		
+			}
         } catch(error) {
-            console.log("While constructing LEDNode widget:", error);		
+            console.log("While constructing LEDNode widget:", error);
 		}
     }
 
